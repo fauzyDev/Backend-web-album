998204ac166ab266d9b5ceba22a7621d952e20ee
fauzyDev <haliffauzy45@gmail.com>
2025-01-07 20:22:23 +0700

    fix

--- src/controller/uploadFile.js
+++ src/controller/uploadFile.js
@@ -4,5 +4,5 @@ import multer from "multer";
 const storage = multer.memoryStorage()
 export const upload = multer({ 
     storage,
-    limits: { fileSize: 10 * 1024 * 1024 }
+    limits: { fileSize: 20 * 1024 * 1024 }
 })
